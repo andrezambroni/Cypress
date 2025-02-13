652d0332ef15fb94d2806739835fee318d4b76f6
andrezambroni <andrezambroni@gmail.com>
2025-02-13 13:56:54 -0300

    teste no formulario de cadastro

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,11 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit("https://adopet-frontend-cypress.vercel.app")
+describe("template spec", () => {
+  it("passes", () => {
+    cy.visit("https://adopet-frontend-cypress.vercel.app/")
+    cy.contains("a", "Cadastrar").click()
+    cy.get('input[name="nome"]').type("Andre Zambroni")
+    cy.get('input[name="email"]').type("andre@email.com")
+    cy.get('input[name="password"]').type("Senha123")
+    cy.get('input[name="confirm_password"]').type("Senha123")
+    cy.contains("button", "Cadastrar").click()
   })
 })

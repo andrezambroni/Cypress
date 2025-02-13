b8efce9acdd0951a7356b3cf1a5ab8854f54f991
andrezambroni <andrezambroni@gmail.com>
2025-02-13 14:14:12 -0300

    feat: add tests navigation

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,11 +1,51 @@
-describe("Página de cadastro", () => {
-  it("Preencher os campos do formulário corretamente para cadastrar um novo usuário", () => {
-    cy.visit("https://adopet-frontend-cypress.vercel.app/")
-    cy.contains("a", "Cadastrar").click()
-    cy.get('input[name="nome"]').type("Andre Zambroni")
-    cy.get('input[name="email"]').type("andre@email.com")
-    cy.get('input[name="password"]').type("Senha123")
-    cy.get('input[name="confirm_password"]').type("Senha123")
-    cy.contains("button", "Cadastrar").click()
-  })
-})
+// describe("Página de cadastro", () => {
+//   it("Preencher os campos do formulário corretamente para cadastrar um novo usuário", () => {
+//     cy.visit("https://adopet-frontend-cypress.vercel.app/")
+//     cy.contains("a", "Cadastrar").click()
+//     cy.get('input[name="nome"]').type("Andre Zambroni")
+//     cy.get('input[name="email"]').type("andre@email.com")
+//     cy.get('input[name="password"]').type("Senha123")
+//     cy.get('input[name="confirm_password"]').type("Senha123")
+//     cy.contains("button", "Cadastrar").click()
+//   })
+// })
+
+// describe("", () => {
+//   it("deve carregar a página corretamente e clicar no botão ‘Ver pets disponíveis para adoção”", () => {
+//     cy.visit("https://adopet-frontend-cypress.vercel.app/")
+//     cy.get(".button").click()
+//   })
+// })
+
+// describe("", () => {
+//   it("Visita a página de principal do AdoPet e testa os botão de home", () => {
+//     cy.visit("https://adopet-frontend-cypress.vercel.app/")
+//     cy.get(".header__home").click()
+//   })
+// })
+
+// describe("", () => {
+//   it("Visita a página de /login do Adopet", () => {
+//     cy.visit("https://adopet-frontend-cypress.vercel.app/login")
+//   })
+// })
+
+// describe("", () => {
+//   it("Visita a página de /home do Adopet", () => {
+//     cy.visit("https://adopet-frontend-cypress.vercel.app/home")
+//   })
+// })
+
+// describe("", () => {
+//   it("Visita a página de /home do AdoPet e clique no botão “Falar com o responsável”", () => {
+//     cy.visit("https://adopet-frontend-cypress.vercel.app/home")
+//     cy.get(".header__message").click()
+//   })
+// })
+
+// describe("", () => {
+//   it("Visita a página de /home do AdoPet e clique no botão “Falar com o responsável”", () => {
+//     cy.visit("https://adopet-frontend-cypress.vercel.app/home")
+//     cy.get(".header__message").click()
+//   })
+// })
